17d2f5e9143e0dcc45e2fe718d579cb3935f1b0c
Alex Lopes Pereira <alexlopespereira@gmail.com>
2021-03-05 09:15:32 -0300

    google

--- src/scrapy/google.py
+++ src/scrapy/google.py
@@ -1,11 +1,22 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from webdriver_manager.chrome import ChromeDriverManager
 
-driver = webdriver.Firefox()
-driver.get('https://www.google.com.br')
+from src.scrapy.util import wait_element
 
-input = driver.find_element_by_xpath("//input[@name='q']")
-input.send_keys("teste")
+driver = webdriver.Chrome(ChromeDriverManager().install())
+
+driver.get('https://www.google.com/')
+input = driver.find_element_by_xpath('//input[@title="Pesquisar"]')
+input.send_keys("minha string de busca")
 input.send_keys(Keys.ENTER)
-print(driver)
 
+# WAIT para esperar carregar os resultados na sua página
+wait_element(driver, by=By.XPATH, by_content='//div[@class="g"]')
+lista = driver.find_elements_by_xpath('//div[@class="g"]')
+for l in lista:
+    print(l)
+    l.find_element_by_xpath("./")
+
+print(driver)
